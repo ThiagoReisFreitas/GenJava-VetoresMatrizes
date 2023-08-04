7df479479f5f70e198c6f1d2df12f1022878e5f1
Thiago Reis <reiscorithi@gmail.com>
2023-08-04 11:22:26 -0300

    Primeira atividade

---
+++ VetoresMatrizes/src/Vetores/Ex01.java
@@ -0,0 +1,29 @@
+package Vetores;
+
+import java.util.Scanner;
+
+public class Ex01
+{
+    public static void main(String[] args)
+    {
+        Scanner leia = new Scanner(System.in);
+        int[] lista = new int[]{2,5,1,3,4,9,7,8,10,6};
+        int x, num;
+
+        System.out.print("Digite um número que você deseja encontrar: ");
+        num = leia.nextInt();
+        for(x=0;x<10;x++)
+        {
+            if(lista[x] == num)
+            {
+                System.out.printf("\nO número %d está localizado na posição: %d", num, x);
+                break;
+            }
+            else if(x == 9)
+            {
+                System.out.printf("\nO número %d não foi encontrado!", num);
+            }
+        }
+
+    }
+}
